d206cb4afc06f50fa0d3ac5d4f4ec3aed81929fd
益昇 石 <ndmc402010104@gmail.com>
2026-06-12 14:27:12 +0800

    更新push邏輯

--- version.js
+++ version.js
@@ -1,10 +1,10 @@
 window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v2.0.1-202606121425",
+  version: "v2.0.2-202606121426",
   major: 2,
   minor: 0,
-  patch: 1,
-  buildTime: "202606121425",
-  updatedAt: "2026-06-12T14:25:00+08:00",
+  patch: 2,
+  buildTime: "202606121426",
+  updatedAt: "2026-06-12T14:26:00+08:00",
   source: "version.js"
 };
