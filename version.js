c4259a4294a959776415db5f60af057ba10e471e
益昇 <ndmc402010104@gmail.com>
2026-06-20 01:43:29 +0800

    修正後台header邏輯

--- version.js
+++ version.js
@@ -1,13 +1,14 @@
 ﻿window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v2.7.7-202606200143",
+  version: "v2.7.8-202606200143",
   major: 2,
   minor: 7,
-  patch: 7,
+  patch: 8,
   buildTime: "202606200143",
-  updatedAt: "2026-06-20T01:43:08+08:00",
+  updatedAt: "2026-06-20T01:43:26+08:00",
   source: "version.js"
 };
+
 
 
 
