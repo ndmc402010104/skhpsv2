cbc7c968cf41d7dc250985f12f843632be2b6e03
益昇 <ndmc402010104@gmail.com>
2026-06-20 20:48:08 +0800

    修改swipe table小斷點

--- version.js
+++ version.js
@@ -1,13 +1,14 @@
 ﻿window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v2.7.9-202606201037",
+  version: "v2.7.10-202606202047",
   major: 2,
   minor: 7,
-  patch: 9,
-  buildTime: "202606201037",
-  updatedAt: "2026-06-20T10:37:56+08:00",
+  patch: 10,
+  buildTime: "202606202047",
+  updatedAt: "2026-06-20T20:47:55+08:00",
   source: "version.js"
 };
+
 
 
 
