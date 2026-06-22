899b6752c346849e68c3f402dfd0844bc2b8787e
益昇 <ndmc402010104@gmail.com>
2026-06-22 21:25:46 +0800

    垃圾codex改壞我的swipe table

--- version.js
+++ version.js
@@ -1,13 +1,14 @@
 ﻿window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v2.7.19-202606222125",
+  version: "v2.7.20-202606222125",
   major: 2,
   minor: 7,
-  patch: 19,
+  patch: 20,
   buildTime: "202606222125",
-  updatedAt: "2026-06-22T21:25:02+08:00",
+  updatedAt: "2026-06-22T21:25:29+08:00",
   source: "version.js"
 };
+
 
 
 
