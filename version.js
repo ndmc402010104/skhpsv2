bdbdd7740006a387b9617e74ad95e4f014cb7e85
益昇 <ndmc402010104@gmail.com>
2026-06-30 15:10:41 +0800

    白畫面覆蓋時靈時不靈，不過功能so far so good

--- version.js
+++ version.js
@@ -1,13 +1,14 @@
 ﻿window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v2.8.4-202606301024",
+  version: "v2.8.5-202606301510",
   major: 2,
   minor: 8,
-  patch: 4,
-  buildTime: "202606301024",
-  updatedAt: "2026-06-30T10:24:57+08:00",
+  patch: 5,
+  buildTime: "202606301510",
+  updatedAt: "2026-06-30T15:10:04+08:00",
   source: "version.js"
 };
+
 
 
 
