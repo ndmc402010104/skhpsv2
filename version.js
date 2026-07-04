17fce7a1848bf8159f2b93b298eda3b24a71838a
益昇 <ndmc402010104@gmail.com>
2026-07-05 00:40:08 +0800

    初步完成QR簽到後台建置

--- version.js
+++ version.js
@@ -1,13 +1,15 @@
 ﻿window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v2.10.0-202607032258",
+  version: "v2.11.1-202607050039",
   major: 2,
-  minor: 10,
-  patch: 0,
-  buildTime: "202607032258",
-  updatedAt: "2026-07-03T22:58:47+08:00",
+  minor: 11,
+  patch: 1,
+  buildTime: "202607050039",
+  updatedAt: "2026-07-05T00:39:56+08:00",
   source: "version.js"
 };
+
+
 
 
 
